b671a67b9f7b54d883dd8187cf12a7f1fee5eba0
Agustin Petrillo <agustinpetrillo1@gmail.com>
2022-11-09 00:16:13 -0300

    changes in design and functionality

--- pages/Cart.jsx
+++ pages/Cart.jsx
@@ -28,63 +28,80 @@ const Cart = () => {
       </Head>
       <Background>
         <Container>
-          <div className="flex flex-col items-center my-5 text-center">
+          <div className="flex flex-col items-center my-5">
             <h1 className="mb-5">Carrito de compras</h1>
             {cartItems.length === 0 ? (
-              <div className="mt-10">
-                <p>El carrito esta vacio</p>
+              <div className="mt-10 flex flex-col items-center max-w-xs">
+                <p>El carrito esta vacio :(</p>
                 <Link href="/">
-                  <button className="rounded-2xl bg-white text-black p-3 hover:scale-105 duration-200 transition-all">
+                  <button className="mt-2 w-full rounded-2xl bg-white text-black p-3 hover:scale-105 duration-200 transition-all">
                     Tienda
                   </button>
                 </Link>
               </div>
             ) : (
-              <div className="gap-3 grid grid-cols-4">
-                {cartItems.map((item) => (
-                  <div className="flex flex-col items-center" key={item.id}>
-                    <Image
-                      src={item.image}
-                      alt="/"
-                      height="230"
-                      width="170"
-                      objectFit="cover"
-                      objectPosition="center"
-                      className="rounded-md"
-                    />
-                    <p className="my-1">Producto: {item.name}</p>
-                    <p>Precio: ${item.price * item.quantity} ARS</p>
-                    <div className="flex mt-2 gap-2">
-                      <p>Cantidad:</p>
-                      <select
-                        value={item.quantity}
-                        onChange={(e) => updateCart(item, e.target.value)}
-                        className="text-black text-sm rounded-sm outline-none"
+              <div className="flex flex-col items-center">
+                <div className="gap-5 grid grid-cols-4">
+                  {cartItems.map((item) => (
+                    <div className="flex flex-col items-center" key={item.id}>
+                      <Image
+                        src={item.image}
+                        alt="/"
+                        height="230"
+                        width="170"
+                        objectFit="cover"
+                        objectPosition="center"
+                        className="rounded-md"
+                      />
+                      <p className="my-1 text-xs">
+                        Producto:{" "}
+                        <span className="text-lg ml-1">{item.name}</span>
+                      </p>
+                      <p className="text-xs">
+                        Precio:{" "}
+                        <span className="text-lg ml-1">
+                          ${item.price * item.quantity} ARS
+                        </span>
+                      </p>
+                      <div className="flex mt-2 gap-2">
+                        <p className="text-xs">Cantidad:</p>
+                        <select
+                          value={item.quantity}
+                          onChange={(e) => updateCart(item, e.target.value)}
+                          className="text-black text-sm rounded-sm outline-none ml-1"
+                        >
+                          {[...Array(item.stock).keys()].map((i) => (
+                            <option key={i + 1} value={i + 1}>
+                              {i + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <button
+                        className="mt-5 bg-white text-black rounded-lg p-3 hover:scale-105 transition-all duration-200"
+                        onClick={() => removeCart(item)}
                       >
-                        {[...Array(item.stock).keys()].map((i) => (
-                          <option key={i + 1} value={i + 1}>
-                            {i + 1}
-                          </option>
-                        ))}
-                      </select>
+                        Quitar
+                      </button>
                     </div>
-                    <button
-                      className="mt-5 bg-white text-black rounded-lg p-3 hover:scale-105 transition-all duration-200"
-                      onClick={() => removeCart(item)}
-                    >
-                      Quitar
-                    </button>
-                  </div>
-                ))}
-                <div className="flex flex-col items-center justify-end">
-                  <p>
-                    Cantidad total:{" "}
-                    {cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  ))}
+                </div>
+                <div className="flex flex-col my-10 max-w-xs">
+                  <p className="text-sm">
+                    Cantidad total:
+                    <span className="text-xl ml-1">
+                      {cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
                   </p>
-                  <p>
-                    Precio total: $
-                    {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                  <p className="text-sm">
+                    Precio total:
+                    <span className="text-xl ml-1">
+                      ${cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                    </span>
                   </p>
+                  <button className="mt-2 bg-white text-black rounded-lg p-3 hover:scale-105 transition-all duration-200">
+                    PAGAR
+                  </button>
                 </div>
               </div>
             )}
