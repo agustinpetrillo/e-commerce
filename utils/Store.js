efbfd5ae886a6e7605d01d30c33a69c2bafd682c
Agustin Petrillo <agustinpetrillo1@gmail.com>
2022-11-29 14:30:19 -0300

    better formatting

--- utils/Store.js
+++ utils/Store.js
@@ -4,43 +4,51 @@ import { useReducer, createContext, useState, useEffect } from "react";
 export const Store = createContext();
 
 const initialState = {
-    cart: {
-        cartItems: []
-    }
-}
+  cart: {
+    cartItems: [],
+  },
+};
 
 const reducer = (state, action) => {
-    switch (action.type) {
-        case "CART_ADD_ITEM": {
-            const newItem = action.payload;
-            const existItem = state.cart.cartItems.find((item) => item.slug === newItem.slug)
-            const cartItems = existItem ? state.cart.cartItems.map((item) => item.name === existItem.name ? newItem : item)
-                : [...state.cart.cartItems, newItem]
-            return {
-                ...state, cart: { ...state.cart, cartItems }
-            }
-        }
-        case "CART_REMOVE_ITEM": {
-            const cartItems = state.cart.cartItems.filter((item) => item.slug !== action.payload.slug)
-            return { ...state, cart: { ...state.cart, cartItems } }
-        }
-
-        default:
-            return state;
+  switch (action.type) {
+    case "CART_ADD_ITEM": {
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item.slug === newItem.slug
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.name === existItem.name ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      return {
+        ...state,
+        cart: { ...state.cart, cartItems },
+      };
     }
-}
+    case "CART_REMOVE_ITEM": {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item.slug !== action.payload.slug
+      );
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
+
+    default:
+      return state;
+  }
+};
 
 export const StoreProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-    const [priceTo, setPriceTo] = useState(true);
-    //true is ARGS, false is USD
-    const [dolar, setDolar] = useState([]);
-    useEffect(() => {
-        axios
-            .get("https://api.bluelytics.com.ar/v2/latest")
-            .then((res) => setDolar(res.data))
-            .catch((err) => console.log(err));
-    })
-    const value = { state, dispatch, priceTo, setPriceTo, dolar, setDolar }
-    return <Store.Provider value={value}>{children}</Store.Provider>
-}
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [priceTo, setPriceTo] = useState(true);
+  //true is ARGS, false is USD
+  const [dolar, setDolar] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://api.bluelytics.com.ar/v2/latest")
+      .then((res) => setDolar(res.data))
+      .catch((err) => console.log(err));
+  });
+  const value = { state, dispatch, priceTo, setPriceTo, dolar, setDolar };
+  return <Store.Provider value={value}>{children}</Store.Provider>;
+};
